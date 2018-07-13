e531a880f7dbaf5a01d32c166522fd3a07941741
michaell308 <michaell308@yahoo.com>
2018-07-12 22:03:34 -0500

    Reset fields when appropriate; fixed bug: asi/pbi should be is_asi/is_pbi

--- public/client.js
+++ public/client.js
@@ -55,24 +55,38 @@ $('#client-form').on('submit', function (event) {
         success: function(data) { //on successful post, data contains anything sent back from server
             console.log('success');
             if (typeof data  === 'string') {
-            	console.log(data);
-            	document.getElementById("error").textContent = data;
+            	document.getElementById("error").textContent = data; //write error
+            	//reset all fields
+            	document.getElementById("hp").textContent = "";
+            	document.getElementById("maxHitDie").textContent = "";
+            	document.getElementById("profBonus").textContent = "";
+            	document.getElementById("spellSaveDC").textContent = "";
+            	$("#features").empty();
+            	$("#spellSlotTable").empty();
+            	document.getElementById("spellAttackMod").textContent = "";
+            	document.getElementById("cantripsKnown").textContent = "";
+            	document.getElementById("spellsKnown").textContent = "";
+				document.getElementById("classOutput").textContent = "";
             }
             else {
+            	document.getElementById("error").textContent = "";
 	            //HP
 	            document.getElementById("hp").textContent = "Roll a d" + data.hit_die + ". Your hp increases by the number you rolled + your constitution modifier";
 	            //Max hit die
 	            document.getElementById("maxHitDie").textContent = "The maximum number of hit die you have is now " + data.level;
 	            //Proficiency bonus
-	            var pbi = data.pbi;
-	            if (pbi === true) {
-	            	document.getElementById("profBonus").textContent = "Your proficiency bonus increases to +" + data.prof_bonus;
+	            var profBonus = document.getElementById("profBonus"); 
+	            profBonus.textContent = ""; //reset proficiency bonus
+	            var is_pbi = data.is_pbi;
+	            if (is_pbi === 1) {
+	            	profBonus.textContent = "Your proficiency bonus increases to +" + data.prof_bonus;
 	            }
 	            //Features
+	            $("#features").empty(); //reset features
 	            var featuresArray = data.features.split(',');
 	            if (featuresArray.length > 0 && featuresArray[0] != "") {
-		            var featureDoc = document.getElementById("features");
-		            featureDoc.textContent = "Features:";
+		          	var featureUL = document.getElementById("features");
+		            featureUL.textContent = "Features:";
 		            for (var i = 0; i < featuresArray.length; i++) {
 		              var node = document.createElement("LI");
 		              var button = document.createElement("BUTTON");
@@ -82,10 +96,11 @@ $('#client-form').on('submit', function (event) {
 		              button.className = "featureButton";
 		              button.value = featuresArray[i];
 		              node.appendChild(button); 
-					  featureDoc.appendChild(node);
+					  featureUL.appendChild(node);
 					}
 				}
 				//Spell Slots Table
+				$("#spellSlotTable").empty(); //reset spell slots table
 				var userClass = data.class;
 				var spellSlotArray = data.spell_slots.split(",");
 				var spellSlotArrayLength = spellSlotArray.length;
@@ -113,26 +128,35 @@ $('#client-form').on('submit', function (event) {
 					table.appendChild(dataRow);
 				}
 				//Spell Save DC and Spell Attack Modifier
-				var asi = data.asi;
+				var is_asi = data.is_asi;
 				var stat_modifier = data.stat_modifier;
-				if (stat_modifier !== null && (asi === true || pbi === true)) {
-					document.getElementById("spellSaveDC").textContent = "Update Spell Save DC = 8 + " + data.prof_bonus +" + " + stat_modifier +" modifier";
-					document.getElementById("spellAttackMod").textContent = "Update Spell Attack Modifier = " + data.prof_bonus +" + " + stat_modifier +" modifier";
+				var spellSaveDC = document.getElementById("spellSaveDC");
+				var spellAttackMod = document.getElementById("spellAttackMod");
+				spellSaveDC.textContent = ""; //reset spell save dc
+				spellAttackMod.textContent = ""; //reset spell attack modifier
+				if (stat_modifier !== null && (is_asi === 1 || is_pbi === 1)) {
+					spellSaveDC.textContent = "Update Spell Save DC = 8 + " + data.prof_bonus +" + " + stat_modifier +" modifier";
+					spellAttackMod.textContent = "Update Spell Attack Modifier = " + data.prof_bonus +" + " + stat_modifier +" modifier";
 				}
 				//Cantrips known
 				var cantrips_known = data.cantrips_known;
+				var cantripsKnown = document.getElementById("cantripsKnown");
+				cantripsKnown.textContent = ""; //reset cantrips known
 				if (cantrips_known !== null) {
-					document.getElementById("cantripsKnown").textContent = "You now know " + cantrips_known + " cantrips";
+					cantripsKnown.textContent = "You now know " + cantrips_known + " cantrips";
 				}
 				//Spells known
 				var spells_known = data.spells_known;
+				var spellsKnown = document.getElementById("spellsKnown");
+				spellsKnown.textContent = ""; //reset spells known
 				if (spells_known !== null) {
-					document.getElementById("spellsKnown").textContent = "You now know " + spells_known + " spells";
+					spellsKnown.textContent = "You now know " + spells_known + " spells";
 				}
 				//Class output
+				var classOutput = document.getElementById("classOutput");
+				classOutput.textContent = ""; //reset class output
 				//Barbarian
 				if (userClass === "Barbarian") {
-					var classOuput = document.getElementById("classOuput");
 					var num_rages = data.num_rages;
 					if (num_rages !== null) {
 						classOuput.textContent = "Number of rages is now = " + num_rages + "\r\n";
@@ -144,19 +168,13 @@ $('#client-form').on('submit', function (event) {
 				}
 				//Bard
 				else if (userClass === "Bard") {
-					var classOuput = document.getElementById("classOuput");
 					var inspiration_die = data.inspiration_die;
 					if (inspiration_die !== null) {
 						classOutput.textContent = "Your inspiration die is now a d" + inspiration_die + "\r\n";
 					}
-
-					if (asi === true) { //this should be moved to right after the features
-						classOutput.textContent += "If you "
-					}
 				}
 				//Monk
 				else if (userClass === "Monk") {
-					var classOuput = document.getElementById("classOuput");
 					var ki_points = data.level;
 					classOuput.textContent = "You now have " + ki_points + " ki points" + "\r\n";
 					var martial_arts_damage = data.martial_arts_damage;
@@ -170,7 +188,6 @@ $('#client-form').on('submit', function (event) {
 				}
 				//Rogue
 				else if (userClass === "Rogue") {
-					var classOuput = document.getElementById("classOuput");
 					var num_sneak_attack_die = data.num_sneak_attack_die;
 					if (num_sneak_attack_die !== null) {
 						classOuput.textContent = "Your sneak attack now does " + num_sneak_attack_die + "d6 damage"; 
@@ -178,13 +195,11 @@ $('#client-form').on('submit', function (event) {
 				}
 				//Sorcerer
 				else if (userClass === "Sorcerer") {
-					var classOuput = document.getElementById("classOuput");
 					var sorcerer_points = data.level;
 					classOuput.textContent = "You now have " + sorcerer_points + " sorcerer points";
 				}
 				//Warlock
 				else if (userClass === "Warlock") {
-					var classOuput = document.getElementById("classOuput");
 					var spell_slots = data.spell_slots;
 					if (spell_slots !== null) {
 						var num_spell_slots = spell_slots.split(",")[0];
